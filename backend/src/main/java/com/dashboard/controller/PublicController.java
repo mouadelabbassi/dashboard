d549f732929a18c36486a99000f54294ace38f29
mouadelabbassi <moad.elabbassi@usms.ac.ma>
2025-12-22 23:28:02 +0100

    commit 31

--- backend/src/main/java/com/dashboard/controller/PublicController.java
+++ backend/src/main/java/com/dashboard/controller/PublicController.java
@@ -11,8 +11,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/public")
